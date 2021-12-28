623dca1cc997d98bea06f305eaeee601bea28599
Mc141 <85196947+Mc141@users.noreply.github.com>
2021-12-28 08:52:46 -0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", (req, res) => {
+    let num1 = req.body.num1;
+    let num2 = req.body.num2;
+
+    let result = Number(num1) + Number(num2);
+
+    res.send(`The relust of the calculation is ${result}.`);
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000.");
+});
